33c3240ba29b29ecbadf6c737e621d389f2daad4
Obaid Ramadan Taha <156338017+ObiedTaha@users.noreply.github.com>
2024-07-03 21:32:58 +0300

    creat component and seperate moduels and finish nav

---
+++ src/app/products/components/all-products/all-products.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-all-products',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './all-products.component.html',
+  styleUrls: ['./all-products.component.scss']
+})
+export class AllProductsComponent {
+
+}
---
+++ src/app/products/components/products-details/products-details.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-products-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './products-details.component.html',
+  styleUrls: ['./products-details.component.scss']
+})
+export class ProductsDetailsComponent {
+
+}
