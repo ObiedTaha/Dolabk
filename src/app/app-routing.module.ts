a2f0e6a69ae0a74f0faaf9136a5c6500dd4a7588
Obaid Ramadan Taha <156338017+ObiedTaha@users.noreply.github.com>
2024-07-08 20:36:11 +0300

    do contacts part

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path:'',redirectTo:'home',pathMatch:'full'},
+  { path: 'register', loadComponent: () => import('./auth/components/register/register.component').then(m => m.RegisterComponent), title: 'Register' },
+  { path: 'login', loadComponent: () => import('./auth/components/login/login.component').then(m => m.LoginComponent), title: 'Login' },
   { path:'home',loadComponent:()=>import('./shared/components/home/home.component').then(m=>m.HomeComponent),title:'Home'},
   { path: 'products', loadComponent: () => import('./products/components/all-products/all-products.component').then(m => m.AllProductsComponent), title: 'Products' },
   { path: 'productDetails/:id', loadComponent: () => import('./products/components/products-details/products-details.component').then(m => m.ProductsDetailsComponent), title: 'Product Details' },
   { path: 'cart', loadComponent: () => import('./carts/components/cart/cart.component').then(m => m.CartComponent), title: 'Cart' },
-  { path: 'register', loadComponent: () => import('./auth/components/register/register.component').then(m => m.RegisterComponent), title: 'Register' },
-  { path: 'login', loadComponent: () => import('./auth/components/login/login.component').then(m => m.LoginComponent), title: 'Login' },
+  {path:'contacts',loadComponent:()=>import('./components/contacts/contacts.component').then(m=>m.ContactsComponent),title:'Contacts'},
   { path: '**', loadComponent: () => import('./shared/components/notfound/notfound.component').then(m => m.NotfoundComponent), title: 'Not Found' }
 ];
 
